d02fea90a9802b81a049a49161f7fe881d408ba5
Peter Binkley <peter.binkley@ualberta.ca>
2021-07-12 15:10:36 -0600

    Incorporate createmanifest into Image class

--- image.py
+++ image.py
@@ -24,64 +24,56 @@ class Image:
             self.url = self.iiifimage
             self.tmpfilepath = False
             self.manifesturl = "{}{}/manifest.json".format(self.origin_url, self.manifestpath)
-            self.manifest = createmanifest(self.tmpfilepath, self.imgurl, self.url, self.iiiffolder, self.request_form, self.manifesturl)
-            if type(self.manifest) == dict:
-                self.template_name = 'upload.html'
-                self.template_vars = {'error': self.manifest['error']}
-            else:
-                # pb: previously self.manifest was overwritten with the string version, but for testing we need the json version
-                self.manifest_markdown = self.manifest.toString(compact=False).replace('canvas/info.json', 'info.json').replace('https://{{site.url}}', '{{site.url}}')
-            self.manifest_markdown = "---\n---\n{}".format(self.manifest_markdown)
+            self.createmanifest()
         else:
             # handle uploaded image
             self.file = request_files['file']
             self.encodedimage = self.file.stream.read()
 
-def createmanifest(tmpfilepath, imgurl, url, iiiffolder, formdata, manifesturl):
-    try:
-        fac = ManifestFactory()
-        fac.set_base_prezi_uri(url)
-        fac.set_base_image_uri(imgurl)
-        fac.set_iiif_image_info(2.0, 2)
-        manifest = fac.manifest(ident=manifesturl, label=formdata['label'])
-        manifest.viewingDirection = formdata['direction']
-        manifest.description = formdata['description']
-        manifest.set_metadata({"rights": formdata['rights']})
-        seq = manifest.sequence()
-        cvs = seq.canvas(ident='info', label=formdata['label'])
-        anno_id = "{}{}/info.json".format(imgurl, iiiffolder)
-        anno = cvs.annotation(ident='anno-id')
-        img = anno.image(iiiffolder, iiif=True)
-    except Exception as e:
-        return {'error': e}
-    if tmpfilepath:
-        img.set_hw_from_file(tmpfilepath)
-    else:
+    def createmanifest(self):
         try:
-            img.set_hw_from_iiif()
-        except:
+            fac = ManifestFactory()
+            fac.set_base_prezi_uri(self.url)
+            fac.set_base_image_uri(self.imgurl)
+            fac.set_iiif_image_info(2.0, 2)
+            manifest = fac.manifest(ident=self.manifesturl, label=self.request_form['label'])
+            manifest.viewingDirection = self.request_form['direction']
+            manifest.description = self.request_form['description']
+            manifest.set_metadata({"rights": self.request_form['rights']})
+            seq = manifest.sequence()
+            cvs = seq.canvas(ident='info', label=self.request_form['label'])
+            anno = cvs.annotation(ident="{}{}/annotation/1.json".format(self.origin_url, self.manifestpath))
+            img = anno.image(self.iiiffolder, iiif=True)
+        except Exception as e:
+            return {'error': e}
+        if self.tmpfilepath:
+            img.set_hw_from_file(self.tmpfilepath)
+        else:
             try:
-                response = requests.get("{}{}/info.json".format(imgurl, iiiffolder))
-                if response.status_code > 299:
-                    response = requests.get("{}{}".format(imgurl, iiiffolder))
-                try:
-                    content = response.json()
-                except:
-                    return {'error': 'No IIIF image exists at {}{}'.format(imgurl, iiiffolder)}
-                    # im = Image.open(BytesIO(response.content))
-                    # print(im.size)
-                    # content = {'height': im.size[1], 'width': im.size[0]}
-                img.height = content['height']
-                img.width = content['width']
+                img.set_hw_from_iiif()
             except:
-                return {'error': 'Unable to get height/width for image located at {}{}'.format(imgurl, iiiffolder)}
-    cvs.height = img.height
-    cvs.width = img.width
-    return manifest
-
-# pb: not used?
-def createjekyllfile(contents, filename, iiifpath):
-    jekyllstring = "---\n---\n{}".format(contents)
-    with open(os.path.join(iiifpath, filename), 'w') as file:
-        file.write(jekyllstring)
-
+                try:
+                    response = requests.get("{}{}/info.json".format(self.imgurl, self.iiiffolder))
+                    if response.status_code > 299:
+                        response = requests.get("{}{}".format(self.imgurl, self.iiiffolder))
+                    try:
+                        content = response.json()
+                    except:
+                        return {'error': 'No IIIF image exists at {}{}'.format(self.imgurl, self.iiiffolder)}
+                        # im = Image.open(BytesIO(response.content))
+                        # print(im.size)
+                        # content = {'height': im.size[1], 'width': im.size[0]}
+                    img.height = content['height']
+                    img.width = content['width']
+                except:
+                    return {'error': 'Unable to get height/width for image located at {}{}'.format(self.imgurl, self.iiiffolder)}
+        cvs.height = img.height
+        cvs.width = img.width
+        self.manifest = manifest
+        if type(self.manifest) == dict: # manifest creation failed
+            self.template_name = 'upload.html'
+            self.template_vars = {'error': self.manifest['error']}
+        else:
+            # pb: previously self.manifest was overwritten with the string version, but for testing we need the json version
+            manifest_markdown = self.manifest.toString(compact=False).replace('canvas/info.json', 'info.json').replace('https://{{site.url}}', '{{site.url}}')
+            self.manifest_markdown = "---\n---\n{}".format(manifest_markdown)
--- test_image.py
+++ test_image.py
@@ -3,9 +3,12 @@ import image as ImageClass
 import requests
 import requests_mock
 import json
+from werkzeug.datastructures import FileStorage
+
+import pdb
 
 class Test(unittest.TestCase):
-   def test_image(self):
+   def test_image_iiif(self):
       request_form = {'upload': 'file:testdata/imagetest', 'label': 'This is the label', 'direction': 'left-to-right', 'description': 'This is the description', 'rights': 'This is the rights'}
       request_files = []
       request_url = 'http://session/origin_url/'
@@ -16,6 +19,7 @@ class Test(unittest.TestCase):
       mfst = image.manifest.toJSON(top=True)
       self.assertEqual(mfst['sequences'][0]['canvases'][0]['width']
 , 1784)
+      self.assertEqual(mfst['sequences'][0]['canvases'][0]['images'][0]['@id'], 'http://session/origin_url/manifests/imagetest/annotation/1.json')
 
 if __name__ == '__main__':
    # begin the unittest.main()
